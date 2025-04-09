a8995959fe2b197f472ec9fb4c04f2685e62368b
Prof-yxy <149079949+Prof-yxy@users.noreply.github.com>
2025-04-09 16:05:37 +0800

    Offical Deque

--- Deque.cpp
+++ Deque.cpp
@@ -1,280 +1,244 @@
 #include <iostream>
-struct Node
-{
-    int val;
-    Node *next;
-    Node *front;
-    Node() : val(0), next(nullptr), front(nullptr) {}
-    Node(int val) : val(val), next(nullptr), front(nullptr) {}
-};
+#include <stdexcept>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
 
+template <typename T>
 class Deque
 {
 private:
-    Node *head;
-    Node *tail;
-    int size;
+    T *elements;       // 就是一个循环一维数组
+    size_t capacity;   // 数组的总容量
+    size_t frontIndex; // deque的前端索引
+    size_t backIndex;  // deque的后端索引
+    size_t size;       // deque中的元素数量
 
 public:
-    Deque();
-    ~Deque();
+    // 构造函数
+    Deque() : elements(nullptr), capacity(0), frontIndex(0), backIndex(0), size(0) {}
 
-    void push_back(int val);
-    void push_front(int val);
-    int getSize();
-    void pop_back();
-    void pop_front();
-    void deleteNode(Node *cur);
-    void deleteVal(int val);
-    void clear();
+    // 析构函数
+    ~Deque()
+    {
+        clear();
+        delete[] elements;
+    }
 
-    void print();
+    // 在deque的前端插入元素
+    void push_front(const T &value)
+    {
+        // 检查是否需要扩展数组容量
+        if (size == capacity)
+        {
+            resize();
+        }
 
-    int operator[](int idx);
-    friend std::ostream &operator<<(std::ostream &os, const Deque &lst);
+        // 计算新的前端索引
+        frontIndex = (frontIndex - 1 + capacity) % capacity;
+
+        // 在新的前端位置插入元素
+        elements[frontIndex] = value;
+
+        // 增加deque的元素数量
+        ++size;
+    }
+
+    // 在deque的后端插入元素
+    void push_back(const T &value)
+    {
+        // 检查是否需要扩展数组容量
+        if (size == capacity)
+        {
+            resize();
+        }
+
+        // 在当前后端位置插入元素
+        elements[backIndex] = value;
+
+        // 计算新的后端索引
+        backIndex = (backIndex + 1) % capacity;
+
+        // 增加deque的元素数量
+        ++size;
+    }
+
+    // 从deque的前端移除元素
+    void pop_front()
+    {
+        // 检查deque是否为空
+        if (size == 0)
+        {
+            throw std::out_of_range("Deque is empty");
+        }
+
+        // 删除元素不需要显式地删除, 以后新追加元素会自动覆盖
+        // 计算新的前端索引
+        frontIndex = (frontIndex + 1) % capacity;
+
+        // 减少deque的元素数量
+        --size;
+    }
+
+    // 从deque的后端移除元素
+    void pop_back()
+    {
+        // 检查deque是否为空
+        if (size == 0)
+        {
+            throw std::out_of_range("Deque is empty");
+        }
+
+        // 删除元素不需要显式地删除, 以后新追加元素会自动覆盖
+        // 计算新的后端索引
+        backIndex = (backIndex - 1 + capacity) % capacity;
+
+        // 减少deque的元素数量
+        --size;
+    }
+    // 随机访问元素
+    T &operator[](int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+        return elements[(frontIndex + index) % capacity];
+    }
+
+    // 获取deque中的元素数量
+    size_t getSize() const
+    {
+        return size;
+    }
+
+    // 清空deque
+    void clear()
+    {
+        while (size > 0)
+        {
+            pop_front();
+        }
+    }
+
+    // 打印deque中的元素
+    void printElements() const
+    {
+        size_t index = frontIndex;
+        for (size_t i = 0; i < size; ++i)
+        {
+            std::cout << elements[index] << " ";
+            index = (index + 1) % capacity;
+        }
+        std::cout << std::endl;
+    }
+
+private:
+    // 扩展数组容量
+    void resize()
+    {
+        // 计算新的容量
+        size_t newCapacity = (capacity == 0) ? 1 : capacity * 2;
+
+        // 创建新的数组
+        T *newElements = new T[newCapacity];
+
+        // 复制元素到新数组
+        size_t index = frontIndex;
+        for (size_t i = 0; i < size; ++i)
+        {
+            newElements[i] = elements[index];
+            index = (index + 1) % capacity;
+        }
+
+        // 释放旧数组的内存
+        delete[] elements;
+
+        // 更新成员变量
+        elements = newElements;
+        capacity = newCapacity;
+        frontIndex = 0;
+        backIndex = size;
+    }
 };
-
-Deque::Deque() : head(nullptr), tail(nullptr), size(0) {}
-
-Deque::~Deque()
-{
-    while (head)
-    {
-        Node *tmp = head;
-        head = head->next;
-        delete tmp;
-    }
-    head = tail = nullptr;
-    size = 0;
-}
-
-void Deque::push_back(int val)
-{
-    if (!head)
-    {
-        head = new Node(val);
-        tail = head;
-    }
-    else
-    {
-        Node *newNode = new Node(val);
-        tail->next = newNode;
-        newNode->front = tail;
-        tail = tail->next;
-    }
-    size++;
-}
-
-void Deque::push_front(int val)
-{
-    if (!head)
-    {
-        head = new Node(val);
-        tail = head;
-    }
-    else
-    {
-        Node *newNode = new Node(val);
-        newNode->next = head;
-        head->front = newNode;
-        head = newNode;
-    }
-    size++;
-}
-
-int Deque::getSize()
-{
-    return size;
-}
-
-void Deque::pop_back()
-{
-    if (size == 0)
-    {
-        return;
-    }
-    else if (head == tail)
-    {
-        delete head;
-        head = tail = nullptr;
-    }
-
-    else
-    {
-        Node *tmp = tail;
-        tail = tail->front;
-        delete tmp;
-        tail->next = nullptr;
-    }
-    size--;
-}
-
-void Deque::pop_front()
-{
-    if (size == 0)
-    {
-        return;
-    }
-    else if (head == tail)
-    {
-        delete head;
-        head = tail = nullptr;
-    }
-    else
-    {
-        Node *tmp = head;
-        head = head->next;
-        delete tmp;
-        head->front = nullptr;
-    }
-    size--;
-}
-
-void Deque::deleteNode(Node *cur)
-{
-    if (cur == head)
-    {
-        pop_front();
-    }
-    else if (cur == tail)
-    {
-        pop_back();
-    }
-    else
-    {
-        Node *cur_front = cur->front;
-        Node *cur_next = cur->next;
-        cur->front->next = cur_next;
-        cur->next->front = cur_front;
-        delete cur;
-        size--;
-    }
-}
-
-void Deque::deleteVal(int val)
-{
-    Node *cur = head;
-    while (cur)
-    {
-        if (cur->val == val)
-        {
-            Node *tmp = cur;
-            cur = cur->next;
-            deleteNode(tmp);
-        }
-        else
-        {
-            cur = cur->next;
-        }
-    }
-}
-
-void Deque::clear()
-{
-    while (head)
-    {
-        Node *tmp = head;
-        head = head->next;
-        delete tmp;
-    }
-    head = tail = nullptr;
-    size = 0;
-}
-
-void Deque::print()
-{
-    if (size == 0)
-    {
-        std::cout << "empty" << std::endl;
-        return;
-    }
-
-    Node *cur = head;
-    while (cur)
-    {
-        std::cout << cur->val << " ";
-        cur = cur->next;
-    }
-    std::cout << std::endl;
-}
-
-int Deque::operator[](int idx)
-{
-    if (idx < 0 || idx >= size)
-    {
-        return -1;
-    }
-    Node *cur = head;
-    while (idx--)
-    {
-        cur = cur->next;
-    }
-    return cur->val;
-}
-
-std::ostream &operator<<(std::ostream &os, const Deque &lst)
-{
-    Node *cur = lst.head;
-    while (cur)
-    {
-        os << cur->val;
-        cur = cur->next;
-    }
-    return os;
-}
 
 int main()
 {
-    Deque list;
-    int n;
-    std::cin >> n;
-    std::string command;
-    while (n--)
+    // 创建一个 Deque 对象
+    Deque<int> myDeque;
+
+    int N;
+    std::cin >> N;
+    // 读走回车
+    getchar();
+    std::string line;
+    // 接收命令
+    for (int i = 0; i < N; i++)
     {
-        std::cin >> command;
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        std::string command;
+        iss >> command;
+        int value;
+
         if (command == "push_back")
         {
-            int val;
-            std::cin >> val;
-            list.push_back(val);
+            iss >> value;
+            myDeque.push_back(value);
         }
-        else if (command == "push_front")
+
+        if (command == "push_front")
         {
-            int val;
-            std::cin >> val;
-            list.push_front(val);
+            iss >> value;
+            myDeque.push_front(value);
         }
-        else if (command == "size")
+
+        if (command == "pop_back")
         {
-            std::cout << list.getSize() << std::endl;
+            if (myDeque.getSize() == 0)
+            {
+                continue;
+            }
+            myDeque.pop_back();
         }
-        else if (command == "print")
+
+        if (command == "pop_front")
         {
-            list.print();
+            if (myDeque.getSize() == 0)
+            {
+                continue;
+            }
+            myDeque.pop_front();
         }
-        else if (command == "get")
+
+        if (command == "clear")
         {
-            int idx;
-            std::cin >> idx;
-            std::cout << list[idx] << std::endl;
+            myDeque.clear();
         }
-        else if (command == "pop_back")
+
+        if (command == "size")
         {
-            list.pop_back();
+            std::cout << myDeque.getSize() << std::endl;
         }
-        else if (command == "pop_front")
+
+        if (command == "get")
         {
-            list.pop_front();
+            iss >> value;
+            std::cout << myDeque[value] << std::endl;
         }
-        else if (command == "remove")
+
+        if (command == "print")
         {
-            int val;
-            std::cin >> val;
-            list.deleteVal(val);
-        }
-        else if (command == "clear")
-        {
-            list.clear();
+            if (myDeque.getSize() == 0)
+            {
+                std::cout << "empty" << std::endl;
+            }
+            else
+            {
+                myDeque.printElements();
+            }
         }
     }
+    return 0;
 }
